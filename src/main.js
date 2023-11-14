5697c8eee780958ab1de7fbc24d39e73acd305d7
m.chilikina <m.chilikina@agatgroup.com>
2023-11-14 14:26:03 +0300

    first commit

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './scss/reset.scss';
-import './scss/style.scss';
-import '/scss/style_tablet.scss';
-import '/scss/style_mobile.scss';
-import {addImask, validityForm} from './js/script';
-
-addImask();
-validityForm();
-
